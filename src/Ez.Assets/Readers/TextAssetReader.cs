f1d42fc294dc97151a36e11e83f69a6c2d88c037
ezequias2d <ezequiasmoises@gmail.com>
2021-11-11 18:41:44 -0300

    Add text asset reader and writer implementations

---
+++ src/Ez.Assets/Readers/TextAssetReader.cs
@@ -0,0 +1,40 @@
+﻿using Ez.Magics;
+using System.IO;
+
+namespace Ez.Assets.Readers
+{
+    /// <summary>
+    /// A reader for text data.
+    /// </summary>
+    public class TextAssetReader : IAssetReader
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="TextAssetReader"/>.
+        /// </summary>
+        public TextAssetReader()
+        {
+            MagicSupport = new MagicSupportSlim("Text", typeof(string));
+        }
+
+        /// <inheritdoc/>
+        public IMagicSupport MagicSupport { get; }
+
+        /// <summary>
+        /// Tries read a text data from stream.
+        /// </summary>
+        /// <typeparam name="T">Only can be <see cref="string"/>.</typeparam>
+        /// <inheritdoc/>
+        public bool TryRead<T>(Stream stream, out T value)
+        {
+            if (typeof(T).IsAssignableFrom(typeof(string)))
+            {
+                using var reader = new StreamReader(stream, leaveOpen: true);
+                value = (T)(object)reader.ReadToEnd();
+                return true;
+            }
+
+            value = default;
+            return false;
+        }
+    }
+}
---
+++ src/Ez.Assets/Writers/TextAssetWriter.cs
@@ -0,0 +1,38 @@
+﻿using Ez.Magics;
+using System.IO;
+
+namespace Ez.Assets.Writers
+{
+    /// <summary>
+    /// A writer for XML documents and data.
+    /// </summary>
+    public class TextAssetWriter : IAssetWriter
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="TextAssetWriter"/>.
+        /// </summary>
+        public TextAssetWriter()
+        {
+            MagicSupport = new MagicSupportSlim("Text", typeof(string));
+        }
+
+        /// <inheritdoc/>
+        public IMagicSupport MagicSupport { get; }
+
+        /// <summary>
+        /// Tries write a text data to stream.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <inheritdoc/>
+        public bool TryWrite<T>(in T value, Stream stream)
+        {
+            if (typeof(T).IsAssignableFrom(typeof(string)))
+            {
+                using var writer = new StreamWriter(stream, leaveOpen: true);
+                writer.Write((string)(object)value);
+                return true;
+            }
+            return false;
+        }
+    }
+}
