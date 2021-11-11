c0c90acca98675192e6eb00d0f86a52cfa28f8cc
ezequias2d <ezequiasmoises@gmail.com>
2021-11-11 18:41:03 -0300

    Add XML asset reader and writer implementations

---
+++ src/Ez.Assets/Readers/XmlAssetReader.cs
@@ -0,0 +1,55 @@
+﻿using Ez.Magics;
+using System.IO;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace Ez.Assets.Readers
+{
+    /// <summary>
+    /// A reader for XML documents and data.
+    /// </summary>
+    public class XmlAssetReader : IAssetReader
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="XmlAssetReader"/>.
+        /// </summary>
+        public XmlAssetReader()
+        {
+            MagicSupport = new MagicSupportSlim("XML Document", typeof(XmlReader), typeof(XmlDocument), typeof(XPathDocument));
+        }
+
+        /// <inheritdoc/>
+        public IMagicSupport MagicSupport { get; }
+
+        /// <summary>
+        /// Tries read a XML document from stream.
+        /// </summary>
+        /// <typeparam name="T">Only can be <see cref="XmlReader"/>, <see cref="XmlDocument"/>, or <see cref="XPathDocument"/>.</typeparam>
+        /// <inheritdoc/>
+        public bool TryRead<T>(Stream stream, out T value)
+        {
+            if (typeof(T).IsAssignableFrom(typeof(XmlReader)))
+            {
+                value = (T)(object)XmlReader.Create(stream);
+                return true;
+            }
+
+            if (typeof(T).IsAssignableFrom(typeof(XmlDocument)))
+            {
+                var doc = new XmlDocument();
+                doc.Load(stream);
+                value = (T)(object)doc;
+                return true;
+            }
+            
+            if (typeof(T).IsAssignableFrom(typeof(XPathDocument)))
+            {
+                value = (T)(object)new XPathDocument(stream);
+                return true;
+            }
+
+            value = default;
+            return false;
+        }
+    }
+}
---
+++ src/Ez.Assets/Writers/XmlAssetWriter.cs
@@ -0,0 +1,57 @@
+﻿using Ez.Magics;
+using System.IO;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace Ez.Assets.Writers
+{
+    /// <summary>
+    /// A writer for XML documents and data.
+    /// </summary>
+    public class XmlAssetWriter : IAssetWriter
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="XmlAssetWriter"/>.
+        /// </summary>
+        public XmlAssetWriter()
+        {
+            MagicSupport = new MagicSupportSlim("XML Document", typeof(XmlReader), typeof(XmlDocument), typeof(XPathDocument));
+        }
+
+        /// <inheritdoc/>
+        public IMagicSupport MagicSupport { get; }
+
+        /// <summary>
+        /// Tries write a XML document to stream.
+        /// </summary>
+        /// <typeparam name="T">Only can be <see cref="XmlReader"/>, <see cref="XmlDocument"/>, or <see cref="XPathDocument"/>.</typeparam>
+        /// <inheritdoc/>
+        public bool TryWrite<T>(in T value, Stream stream)
+        {
+            if (typeof(T).IsAssignableFrom(typeof(XmlWriter)))
+            {
+                var reader = (XmlReader)(object)value;
+                using var writer = XmlWriter.Create(stream);
+                writer.WriteNode(reader, true);
+                return true;
+            }
+
+            if (typeof(T).IsAssignableFrom(typeof(XmlDocument)))
+            {
+                var doc = (XmlDocument)(object)value;
+                doc.Save(stream);
+                return true;
+            }
+
+            if (typeof(T).IsAssignableFrom(typeof(XPathDocument)))
+            {
+                var xdoc = (XPathDocument)(object)value;
+                using var writer = XmlWriter.Create(stream);
+                var navigator = xdoc.CreateNavigator();
+                writer.WriteNode(navigator, true);
+                return true;
+            }
+            return false;
+        }
+    }
+}
