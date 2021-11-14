96d47feee2a7a5169ee7e9208283ae100a094ca1
ezequias2d <ezequiasmoises@gmail.com>
2021-11-13 22:09:28 -0300

    Add StreamAssetReader and StreamAssetWriter

---
+++ src/Ez.Assets/Readers/StreamAssetReader.cs
@@ -0,0 +1,41 @@
+﻿using Ez.Magics;
+using System;
+using System.IO;
+
+namespace Ez.Assets.Readers
+{
+    /// <summary>
+    /// A reader for stream.
+    /// </summary>
+    public class StreamAssetReader : IAssetReader
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="StreamAssetReader"/>.
+        /// </summary>
+        public StreamAssetReader()
+        {
+            MagicSupport = new MagicSupportSlim("Stream", typeof(Stream));
+        }
+
+        /// <inheritdoc/>
+        public IMagicSupport MagicSupport { get; }
+
+        /// <summary>
+        /// Tries read a stream.
+        /// </summary>
+        /// <inheritdoc/>
+        public bool TryRead(in Stream stream, in Type type, out object value)
+        {
+            if (type.IsAssignableFrom(typeof(Stream)))
+            {
+                var s = new MemoryStream();
+                stream.CopyTo(s);
+                s.Position = 0;
+                value = s;
+                return true;
+            }
+            value = null;
+            return false;
+        }
+    }
+}
---
+++ src/Ez.Assets/Writers/StreamAssetWriter.cs
@@ -0,0 +1,40 @@
+﻿using Ez.Magics;
+using System;
+using System.IO;
+
+namespace Ez.Assets.Writers
+{
+    /// <summary>
+    /// A writer for stream.
+    /// </summary>
+    public class StreamAssetWriter : IAssetWriter
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="StreamAssetWriter"/>.
+        /// </summary>
+        public StreamAssetWriter()
+        {
+            MagicSupport = new MagicSupportSlim("Stream", typeof(Stream));
+        }
+
+        /// <inheritdoc/>
+        public IMagicSupport MagicSupport { get; }
+
+        /// <summary>
+        /// Tries write a stream.
+        /// </summary>
+        /// <inheritdoc/>
+        public bool TryWrite(in object value, in Type type, Stream stream)
+        {
+            if (type.IsAssignableFrom(typeof(Stream)))
+            {
+                if (value is not Stream src)
+                    return false;
+
+                src.CopyTo(stream);
+                return true;
+            }
+            return false;
+        }
+    }
+}
